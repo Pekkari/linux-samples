a0611d1752369dce005f5e0a49bf9c4c64a18f27
José Pekkarinen <koalinux@gmail.com>
2021-03-16 11:19:21 +0200

    Add sysinfo sample
    This commit adds the sysinfo module
    that will show pieces of uname information
    in the kernel log.
    
    Signed-off-by: José Pekkarinen <koalinux@gmail.com>

---
+++ lab2/README.md
@@ -0,0 +1,5 @@
+### sys info
+
+In this module I'll extract some of the
+sys info that regularly is shown in the uname
+command directly from the kernel internals.
---
+++ lab2/sysinfo.c
@@ -0,0 +1,33 @@
+#include <linux/module.h>
+#include <linux/init.h>
+#include <linux/utsname.h>
+
+struct new_utsname *uname;
+
+static int __init sysinfo_init(void)
+{
+    pr_info("Loading sysinfo module");
+    uname = init_utsname();
+    pr_info(
+        "Sys: %s\n Node: %s\n Release: %s\n Version: %s\n Machine: %s\n Domain: %s\n",
+        uname->sysname,
+        uname->nodename,
+        uname->release,
+        uname->version,
+        uname->machine,
+        uname->domainname
+    );
+    return 0;
+}
+
+static void __exit sysinfo_exit(void)
+{
+    pr_info("Unloading sysinfo module");
+    return;
+}
+
+module_init(sysinfo_init);
+module_exit(sysinfo_exit);
+
+MODULE_AUTHOR("Jose Pekkarinen");
+MODULE_LICENSE("GPL v2");
